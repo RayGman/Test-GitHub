f0f9204a90b7ad42770cd9e683ec952dd89bd2a6
Artemii Zheludkov <artmjeludkov@gmail.com>
2022-12-11 09:31:56 +0200

    cube

---
+++ Scripts/Cube/CubeView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScrollShooter
+{
+    public class CubeView : MonoBehaviour
+    {
+        [SerializeField] private float _health;
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage));
+
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
