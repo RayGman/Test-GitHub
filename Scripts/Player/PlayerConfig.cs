946af6fcb62b94c97e359b3bedd1706e7048f755
Artemii Zheludkov <artmjeludkov@gmail.com>
2022-12-11 10:39:49 +0200

    change config

--- Scripts/Player/PlayerConfig.cs
+++ Scripts/Player/PlayerConfig.cs
@@ -6,7 +6,5 @@ namespace ScrollShooter
     [CreateAssetMenu(fileName = "newPlayer", menuName = "Configs/PlayerConfig", order = 1)]
     public class PlayerConfig : ScriptableObject
     {
-        [SerializeField] public float MoveSpeed;
-        [SerializeField] public float AttackSpeed;
     }
 }
